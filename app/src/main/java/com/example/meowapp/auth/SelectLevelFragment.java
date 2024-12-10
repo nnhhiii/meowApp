db8551a3525083cd6f75ff396b4ff3affeba29ef
cherriedy <huy5adv@gmail.com>
2024-12-10 13:12:53 +0700

    update logup feature

--- app/src/main/java/com/example/meowapp/auth/SelectLevelFragment.java
+++ app/src/main/java/com/example/meowapp/auth/SelectLevelFragment.java
@@ -24,6 +24,8 @@ import com.example.meowapp.api.FirebaseApiService;
 import com.example.meowapp.model.LanguagePreference;
 import com.example.meowapp.model.Level;
 import com.example.meowapp.model.User;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,6 +35,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -75,8 +78,8 @@ public class SelectLevelFragment extends Fragment {
         languagePreferenceReference = FirebaseDatabase.getInstance().getReference("language_preferences");
 
         getReceivingBundle();
-
         loadLevelSelections();
+
         listView.setOnItemClickListener((parent, view1, position, id) -> processingSelectedItem(position));
         btnSubmit.setOnClickListener(v -> processingSubmitOnClick());
     }
@@ -92,110 +95,6 @@ public class SelectLevelFragment extends Fragment {
             ).show();
             getParentFragmentManager().popBackStack();
         }
-    }
-
-    private void processingSelectedItem(int position) {
-        Map.Entry<String, Level> selectedEntry = adapter.getItem(position);
-        if (!TextUtils.isEmpty(selectedEntry.getKey())) {
-            args.putString("level_id", selectedEntry.getKey());
-        } else {
-            Log.e(TAG, "Can't get selected level id");
-            Toast.makeText(
-                    requireContext(),
-                    "Lỗi hệ thống, vui lòng thử lại sau!",
-                    Toast.LENGTH_SHORT
-            ).show();
-            btnSubmit.setEnabled(false);
-        }
-    }
-
-    private void processingSubmitOnClick() {
-        progressBar.setVisibility(View.VISIBLE);
-        selectLevelForm.setVisibility(View.GONE);
-
-        processingRegisterAccount(args);
-
-        Intent intent = new Intent(requireContext(), MainActivity.class);
-        intent.setAction(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        startActivity(intent);
-    }
-
-    private void processingRegisterAccount(Bundle args) {
-        String email = args.getString("email");
-        String password = args.getString("password");
-        String username = args.getString("username", "unknown");
-        String language_id = args.getString("language_id");
-        String level_id = args.getString("level_id");
-        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
-            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(requireActivity(), task -> {
-                        if (task.isSuccessful()) {
-                            Log.i(TAG, "Create account with Firebase Authentication successfully!");
-                            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-                            if (currentUser != null) {
-                                processingUserData(currentUser.getUid(), username, language_id, level_id);
-                            } else {
-                                Log.e(TAG, "Can't obtain FirebaseUser object");
-                                Toast.makeText(requireContext(), "Lỗi hệ thống đăng ký!", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Exception exception = task.getException();
-                            if (exception instanceof FirebaseAuthException) {
-                                String errorCode = ((FirebaseAuthException) exception).getErrorCode();
-                                switch (errorCode) {
-                                    case "ERROR_EMAIL_ALREADY_IN_USE":
-                                        Toast.makeText(requireContext(), "Email đã được đăng ký!", Toast.LENGTH_SHORT).show();
-                                        break;
-                                    case "ERROR_INVALID_EMAIL":
-                                        Toast.makeText(requireContext(), "Email không đúng định dạng!", Toast.LENGTH_SHORT).show();
-                                        break;
-                                    case "ERROR_WEAK_PASSWORD":
-                                        Toast.makeText(requireContext(), "Mật khẩu quá yếu!", Toast.LENGTH_SHORT).show();
-                                        break;
-                                    default:
-                                        Log.e(TAG, "FireBase Error Code: " + errorCode);
-                                        break;
-                                }
-                            } else {
-                                Log.e(TAG, "Create account with Firebase Authentication fail!", exception);
-                            }
-                        }
-                    });
-        } else {
-            Log.e(TAG, "Can't get email and password from bundle");
-            Toast.makeText(requireContext(), "Lỗi hệ thống!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void processingUserData(String uid, String username, String language_id, String level_id) {
-        User user = new User();
-        user.setEmail(firebaseAuth.getCurrentUser().getEmail());
-        user.setUsername(username);
-        user.setRole("user");
-        user.setLanguage_id(language_id);
-        user.setCreated_at("");
-        user.setUpdated_at("");
-
-        usersReference.child(uid).setValue(user).addOnCompleteListener(saveUserDataTask -> {
-            if (saveUserDataTask.isSuccessful()) {
-                Log.i(TAG, "Create user data successfully!");
-                LanguagePreference preference = new LanguagePreference(language_id, uid, level_id, 0);
-                languagePreferenceReference.child("test").setValue(preference)
-                        .addOnCompleteListener(saveLanguageReferenceTask -> {
-                            if (saveLanguageReferenceTask.isSuccessful()) {
-                                Log.i(TAG, "Save language reference successfully!");
-                                Toast.makeText(requireContext(), "Đăng kí tài khoản thành công!", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Log.e(TAG, "Save language reference fail!", saveLanguageReferenceTask.getException());
-                                Toast.makeText(requireContext(), "Đăng kí tài khoản thất bại!", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            } else {
-                Log.e(TAG, "Create user data fail!", saveUserDataTask.getException());
-                Toast.makeText(requireContext(), "Đăng kí tài khoản thất bại!", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     private void loadLevelSelections() {
@@ -221,4 +120,126 @@ public class SelectLevelFragment extends Fragment {
                     }
                 });
     }
+
+    private void processingSelectedItem(int position) {
+        Map.Entry<String, Level> selectedEntry = adapter.getItem(position);
+        if (!TextUtils.isEmpty(selectedEntry.getKey())) {
+            args.putString("level_id", selectedEntry.getKey());
+        } else {
+            Log.e(TAG, "Can't get selected level id");
+            Toast.makeText(
+                    requireContext(),
+                    "Lỗi hệ thống, vui lòng thử lại sau!",
+                    Toast.LENGTH_SHORT
+            ).show();
+            btnSubmit.setEnabled(false);
+        }
+    }
+
+    private void navigationToMainActivity() {
+        Intent intent = new Intent(requireContext(), MainActivity.class);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(intent);
+    }
+
+    private void processingSubmitOnClick() {
+        progressBar.setVisibility(View.VISIBLE);
+        selectLevelForm.setVisibility(View.GONE);
+
+        processingRegisterAccount(args, this::navigationToMainActivity);
+    }
+
+    private void processingRegisterAccount(Bundle args, Runnable onSuccessCallback) {
+        String email = args.getString("email");
+        String password = args.getString("password");
+        String username = args.getString("username", "unknown");
+        String language_id = args.getString("language_id");
+        String level_id = args.getString("level_id");
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Log.e(TAG, "Can't get email or password from bundle");
+            Toast.makeText(requireContext(), "Lỗi hệ thống!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        Log.i(TAG, "Create account with Firebase Authentication successfully!");
+                        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                        if (currentUser == null) {
+                            Log.e(TAG, "Can't obtain FirebaseUser object");
+                            Toast.makeText(requireContext(), "Lỗi hệ thống đăng ký!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        processingUserData(currentUser.getUid(), username, language_id, level_id, onSuccessCallback);
+                    } else {
+                        Exception exception = task.getException();
+                        if (exception instanceof FirebaseAuthException) {
+                            String errorCode = ((FirebaseAuthException) exception).getErrorCode();
+                            mappingErrorCode(errorCode);
+                        } else {
+                            Log.e(TAG, "Create account with Firebase Authentication fail!", exception);
+                        }
+                    }
+                });
+    }
+
+    private void mappingErrorCode(String errorCode) {
+        switch (errorCode) {
+            case "ERROR_EMAIL_ALREADY_IN_USE":
+                Toast.makeText(requireContext(), "Email đã được đăng ký!", Toast.LENGTH_SHORT).show();
+                return;
+            case "ERROR_INVALID_EMAIL":
+                Toast.makeText(requireContext(), "Email không đúng định dạng!", Toast.LENGTH_SHORT).show();
+                return;
+            case "ERROR_WEAK_PASSWORD":
+                Toast.makeText(requireContext(), "Mật khẩu quá yếu!", Toast.LENGTH_SHORT).show();
+                return;
+            default:
+                Log.e(TAG, "FireBase Error Code: " + errorCode);
+        }
+    }
+
+    private void processingUserData(String uid, String username, String language_id, String level_id, Runnable onSuccessCallback) {
+        String defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/appngonngu.appspot.com/o/default_user.jpg?alt=media&token=9861418d-58db-42c0-837e-8f2fe758f981";
+        User user = new User();
+        user.setEmail(firebaseAuth.getCurrentUser().getEmail());
+        user.setUsername(username);
+        user.setAvatar(defaultAvatar);
+        user.setRole("user");
+        user.setLanguage_id(language_id);
+        user.setCreated_at("");
+        user.setUpdated_at("");
+
+        usersReference.child(uid).setValue(user).addOnCompleteListener(saveUserDataTask -> {
+            if (saveUserDataTask.isSuccessful()) {
+                Log.i(TAG, "Create user data successfully!");
+                LanguagePreference preference = new LanguagePreference(language_id, uid, level_id, 0);
+                languagePreferenceReference.child("test").setValue(preference)
+                        .addOnCompleteListener(saveLanguageReferenceTask -> {
+                            if (saveLanguageReferenceTask.isSuccessful()) {
+                                Log.i(TAG, "Save language reference successfully!");
+                                Toast.makeText(requireContext(), "Đăng kí tài khoản thành công!", Toast.LENGTH_SHORT).show();
+                                if (onSuccessCallback != null) {
+                                    onSuccessCallback.run();
+                                } else {
+                                    Log.e(TAG, "Not Found onSuccessCallback");
+                                    Toast.makeText(requireContext(), "Lỗi hệ thống!", Toast.LENGTH_SHORT).show();
+                                    requireActivity().getSupportFragmentManager().popBackStackImmediate();
+                                }
+                            } else {
+                                Log.e(TAG, "Save language reference fail!", saveLanguageReferenceTask.getException());
+                                Toast.makeText(requireContext(), "Đăng kí tài khoản thất bại!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } else {
+                Log.e(TAG, "Create user data fail!", saveUserDataTask.getException());
+                Toast.makeText(requireContext(), "Đăng kí tài khoản thất bại!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
